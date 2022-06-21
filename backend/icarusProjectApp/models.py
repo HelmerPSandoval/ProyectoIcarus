3c92d9882385f7fd99b956efe6271280ec3e8c18
alesmms <almma.laoi@gmail.com>
2022-06-21 18:58:25 -0400

    fix: Arreglos en modelos.
    Se realizaron arreglos en los fields de los modelos Reserva, Pago, Ciudad, Avion, Vuelo.

--- backend/icarusProjectApp/models.py
+++ backend/icarusProjectApp/models.py
@@ -5,13 +5,13 @@ from re import T
 from django.db import models
 
 class Usuario(models.Model):
-    rut = models.CharField(primary_key = True, max_length=50)
-    nombre = models.CharField(max_length=255)
-    apellido = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    password = models.CharField(max_length=255)
+    rut = models.CharField(primary_key = True, max_length = 255, unique = True)
+    nombre = models.CharField(max_length = 255)
+    apellido = models.CharField(max_length = 255)
+    email = models.EmailField(max_length = 255)
+    password = models.CharField(max_length = 255)
     rol = models.IntegerField()
-    sexo = models.CharField(max_length=255)
+    sexo = models.CharField(max_length = 255)
     telefono = models.IntegerField()
     fecha_nacimiento = models.DateField()
     
@@ -19,7 +19,7 @@ class Usuario(models.Model):
         managed = True
 
 class Reserva(models.Model):
-    id = models.IntegerField(primary_key = True)
+    id = models.AutoField(primary_key = True, unique = True)
     solo_ida = models.BooleanField()
     fecha_reserva = models.DateField()
     valor_reserva = models.IntegerField()
@@ -32,8 +32,8 @@ class Reserva(models.Model):
         managed = True
 
 class Pago(models.Model):
-    id = models.IntegerField(primary_key = True)
-    tarjeta_credito = models.CharField(max_length=255)
+    id = models.AutoField(primary_key = True, unique = True)
+    tarjeta_credito = models.CharField(max_length = 255)
     numero_tarjeta = models.BigIntegerField()
     fecha_nacimiento = models.DateField()
     cvc = models.IntegerField()
@@ -50,23 +50,23 @@ class Pago(models.Model):
         managed = True
 
 class Ciudad(models.Model):
-    id = models.IntegerField(primary_key = True) 
-    nombre = models.CharField(max_length=255)
-    pais = models.CharField(max_length=255)
+    id = models.AutoField(primary_key = True, unique = True)
+    nombre = models.CharField(max_length = 255)
+    pais = models.CharField(max_length = 255)
 
     class Meta:
         managed = True
 
 class Avion(models.Model):
-    id = models.IntegerField(primary_key = True)
-    modelo = models.CharField(max_length=255)
+    id = models.AutoField(primary_key = True, unique = True)
+    modelo = models.CharField(max_length = 255)
     capacidad = models.IntegerField()
 
     class Meta:
         managed = True
 
 class Vuelo(models.Model):
-    id = models.IntegerField(primary_key = True)
+    id = models.AutoField(primary_key = True, unique = True)
     fecha_salida = models.DateField()
     fecha_llegada = models.DateField()
     id_ciudad_origen = models.ForeignKey(
