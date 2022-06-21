619a1c1920e81f2ac222c4961c01a03f0359b95e
alesmms <almma.laoi@gmail.com>
2022-06-21 00:36:44 -0400

    feat: Creación de app y modelos.
    Se crearon los modelos, pero falta encriptar información de Usuario y Pago.

---
+++ backend/icarusProjectApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IcarusprojectappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'icarusProjectApp'
---
+++ backend/icarusProjectApp/models.py
@@ -0,0 +1,101 @@
+from multiprocessing import managers
+from pyexpat import model
+from re import T
+
+from django.db import models
+
+class Usuario(models.Model):
+    rut = models.CharField(primary_key = True, max_length=50)
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    rol = models.IntegerField()
+    sexo = models.CharField(max_length=255)
+    telefono = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+    
+    class Meta:
+        managed = True
+
+class Reserva(models.Model):
+    id = models.IntegerField(primary_key = True)
+    solo_ida = models.BooleanField()
+    fecha_reserva = models.DateField()
+    valor_reserva = models.IntegerField()
+    rut_usuario = models.ForeignKey(
+        Usuario, 
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        managed = True
+
+class Pago(models.Model):
+    id = models.IntegerField(primary_key = True)
+    tarjeta_credito = models.CharField(max_length=255)
+    numero_tarjeta = models.BigIntegerField()
+    fecha_nacimiento = models.DateField()
+    cvc = models.IntegerField()
+    rut_usuario = models.ForeignKey(
+        Usuario, 
+        on_delete=models.CASCADE
+    )
+    id_reserva = models.ForeignKey(
+        Reserva,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        managed = True
+
+class Ciudad(models.Model):
+    id = models.IntegerField(primary_key = True) 
+    nombre = models.CharField(max_length=255)
+    pais = models.CharField(max_length=255)
+
+    class Meta:
+        managed = True
+
+class Avion(models.Model):
+    id = models.IntegerField(primary_key = True)
+    modelo = models.CharField(max_length=255)
+    capacidad = models.IntegerField()
+
+    class Meta:
+        managed = True
+
+class Vuelo(models.Model):
+    id = models.IntegerField(primary_key = True)
+    fecha_salida = models.DateField()
+    fecha_llegada = models.DateField()
+    id_ciudad_origen = models.ForeignKey(
+        Ciudad,
+        on_delete=models.CASCADE
+    )
+    id_ciudad_destino = models.ForeignKey(
+        Ciudad,
+        on_delete=models.CASCADE
+    )
+    id_avion_asociado = models.ForeignKey(
+        Avion,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        managed = True
+
+class Reserva_Vuelo (models.Model):
+    id_reserva = models.ForeignKey(
+        Reserva,
+        on_delete=models.CASCADE
+    )
+    id_vuelo = models.ForeignKey(
+        Vuelo,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        managed = True
+
+
